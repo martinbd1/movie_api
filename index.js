3ce4c8f8b7326bf8609399b5373b26e5302468e9
Brian Martin <martinbd1@gmail.com>
2022-04-27 18:37:06 -0400

    Update index.js

--- index.js
+++ index.js
@@ -13,7 +13,20 @@ const {
 } = require('express-validator');
 
 const cors = require('cors');
-app.use(cors());
+// app.use(cors());
+
+let allowedOrigins = ['http://localhost:8080', 'http://localhost:1234', 'https://myflixapp1987.herokuapp.com'];
+
+app.use(cors({
+    origin: (origin, callback) => {
+        if (!origin) return callback(null, true);
+        if (allowedOrigins.indexOf(origin) === -1) { // If a specific origin isn’t found on the list of allowed origins
+            let message = 'The CORS policy for this application does not allow access from origin ' + origin;
+            return callback(new Error(message), false);
+        }
+        return callback(null, true);
+    }
+}));
 
 const Movies = Models.Movie;
 const Users = Models.User;
