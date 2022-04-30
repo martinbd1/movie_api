5d88a2e66bf30d837f615bcf0ff879c0462a7638
Brian Martin <martinbd1@gmail.com>
2022-04-29 20:55:49 -0400

    Update index.js

--- index.js
+++ index.js
@@ -13,20 +13,20 @@ const {
 } = require('express-validator');
 
 const cors = require('cors');
-// app.use(cors());  //lets all domain allowed
+app.use(cors()); //lets all domain allowed
 
-let allowedOrigins = ['http://localhost:8080', 'http://localhost:1234', 'https://myflixapp1987.herokuapp.com/movies', 'https://res.cloudinary.com/'];
+// let allowedOrigins = ['http://localhost:8080', 'http://localhost:1234', 'https://myflixapp1987.herokuapp.com/movies', 'https://res.cloudinary.com/'];
 
-app.use(cors({
-    origin: (origin, callback) => {
-        if (!origin) return callback(null, true);
-        if (allowedOrigins.indexOf(origin) === -1) { // If a specific origin isn’t found on the list of allowed origins
-            let message = 'The CORS policy for this application does not allow access from origin ' + origin;
-            return callback(new Error(message), false);
-        }
-        return callback(null, true);
-    }
-}));
+// app.use(cors({
+//     origin: (origin, callback) => {
+//         if (!origin) return callback(null, true);
+//         if (allowedOrigins.indexOf(origin) === -1) { // If a specific origin isn’t found on the list of allowed origins
+//             let message = 'The CORS policy for this application does not allow access from origin ' + origin;
+//             return callback(new Error(message), false);
+//         }
+//         return callback(null, true);
+//     }
+// }));
 
 const Movies = Models.Movie;
 const Users = Models.User;
